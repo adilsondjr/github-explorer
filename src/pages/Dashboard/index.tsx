b3d976b8ba65edfaeba6e5fdcec847616000cc2d
Adilson Jr <adilsondjr@gmail.com>
2020-11-01 22:01:10 -0300

    Create Dashboard page with components and styles

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,64 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi'
 
-import { Title } from './styles'
+import logoImg from '../../assets/logo.svg'
+import Repository from '../Repository';
+
+import { Title, Form, Repositories } from './styles'
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>
+    return (
+        <>
+            <img src={logoImg} alt="Github Explorer" />
+            <Title>Explore repositórios no Github</Title>
+
+            <Form action="">
+                <input type="text" placeholder="Digite o nome do repositorio" />
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <a href="teste">
+                    <img
+                        src="https://avatars2.githubusercontent.com/u/49343939?s=460&u=5884ecf5b61dd97d309479c3edee136c21efd1d1&v=4"
+                        alt="Adilson Domiciano Jr"
+                    /> 
+                    <div>
+                        <strong>adilsondjr/github-explorer</strong>
+                        <p>Explorador de repositórios</p>
+                    </div> 
+
+                    <FiChevronRight size={20} />
+                </a>
+
+                <a href="teste">
+                    <img
+                        src="https://avatars2.githubusercontent.com/u/49343939?s=460&u=5884ecf5b61dd97d309479c3edee136c21efd1d1&v=4"
+                        alt="Adilson Domiciano Jr"
+                    /> 
+                    <div>
+                        <strong>adilsondjr/github-explorer</strong>
+                        <p>Explorador de repositórios</p>
+                    </div> 
+
+                    <FiChevronRight size={20} />
+                </a>
+
+                <a href="teste">
+                    <img
+                        src="https://avatars2.githubusercontent.com/u/49343939?s=460&u=5884ecf5b61dd97d309479c3edee136c21efd1d1&v=4"
+                        alt="Adilson Domiciano Jr"
+                    /> 
+                    <div>
+                        <strong>adilsondjr/github-explorer</strong>
+                        <p>Explorador de repositórios</p>
+                    </div> 
+
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories>
+        </>
+    );
 }
 
 export default Dashboard

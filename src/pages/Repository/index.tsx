7b24e79b58102958cadc60630e2843eedd66b78d
Adilson Jr <adilsondjr@gmail.com>
2020-11-02 12:46:24 -0300

    Create style for Repository page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom'
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Link, useRouteMatch } from 'react-router-dom'
+
+import { Header, RepositoryInfo, Issues } from './styles'
+
+import logoImg from '../../assets/logo.svg';
 
 interface RepositoryParams {
     repository: string;
@@ -8,7 +13,54 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
     const { params } = useRouteMatch<RepositoryParams>();
 
-return <h1>Repository: {params.repository}</h1>
+    return (
+        <>
+            <Header>
+                <img src={logoImg} alt="Github Explorer" />
+                <Link to="/">
+                    <FiChevronLeft size={16} />
+                    Voltar
+                </Link>
+            </Header>
+
+            <RepositoryInfo>
+                <header>
+                    <img src="https://avatars2.githubusercontent.com/u/49343939?s=460&u=5884ecf5b61dd97d309479c3edee136c21efd1d1&v=4" 
+                         alt="Adilson Domiciano Jr" />
+                    <div>
+                        <strong>adilsondjr/github-explorer</strong>
+                        <p>Explorador de repositórios</p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong>1808</strong>
+                        <span>Stars</span>
+                    </li>
+                    <li>
+                        <strong>48</strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong>16</strong>
+                        <span>Issues Abertas</span>
+                    </li>
+                </ul>
+            </RepositoryInfo>
+
+            <Issues>
+                <Link to="asas">
+
+                    <div>
+                        <strong>asa</strong>
+                        <p>asas</p>
+                    </div> 
+
+                    <FiChevronRight size={20} />
+                </Link>
+            </Issues>
+        </>
+    )
 }
 
 export default Repository
